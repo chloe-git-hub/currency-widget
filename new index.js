59900b817f05b31fa753d304eb94641bb26a3a34
chloe-git-hub <xiaoqi@d-ron.com>
2025-12-15 14:53:58 +0800

    Update new index.js

--- new index.js
+++ new index.js
@@ -1,10 +1,38 @@
 window.onload = function () {
     ZOHO.embeddedApp.init().then(() => {
 
-        alert("SDK INIT OK");
+        const btn = document.getElementById("confirmBtn");
+        const currencyEl = document.getElementById("currency");
+        const msg = document.getElementById("message");
 
-        document.getElementById("confirmBtn").onclick = function () {
-            alert("BUTTON CLICKED");
+        btn.onclick = async function () {
+            msg.innerText = "";
+
+            const currency = currencyEl.value;
+            if (!currency) {
+                msg.innerText = "Please select a currency.";
+                return;
+            }
+
+            btn.disabled = true;
+            btn.innerText = "Opening Quote…";
+
+            try {
+                await ZOHO.CRM.UI.Record.create({
+                    Entity: "Quotes",
+                    prefill: {
+                        Currency: currency
+                    }
+                });
+
+                ZOHO.embeddedApp.close();
+
+            } catch (e) {
+                console.error(e);
+                msg.innerText = "Unable to open Quote form.";
+                btn.disabled = false;
+                btn.innerText = "Confirm";
+            }
         };
 
     });
