88e7557e0a4c89ac2a634a1079c071b61325adb0
chloe-git-hub <xiaoqi@d-ron.com>
2025-12-15 10:55:48 +0800

    Create new index.js

---
+++ new index.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    alert("Window loaded");
+
+    ZOHO.embeddedApp.init().then(() => {
+        alert("SDK INITIALIZED");
+    }).catch(e => {
+        alert("SDK FAILED");
+        console.error(e);
+    });
+};
