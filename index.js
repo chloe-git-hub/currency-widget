ed275fae3f1a427fc0702c82bbb0ed3b6be7ae63
chloe-git-hub <xiaoqi@d-ron.com>
2025-12-15 10:45:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 confirmBtn.onclick = function () {
     alert("BUTTON CLICKED");
 };
+
+window.onload = function () {
+    alert("Window loaded");
+
+    ZOHO.embeddedApp.init().then(() => {
+        alert("SDK INITIALIZED");
+    }).catch(e => {
+        alert("SDK FAILED");
+        console.error(e);
+    });
+};
+
+
